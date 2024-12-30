f7862afdc27d8dfe414aec6616097d2808fbd4b0
Nurudeen Adebileje <Nuruadebileje@gmail.com>
2024-12-30 15:53:23 -0600

    Courses Templates

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+# # <<<<<<< HEAD
+# #!/bin/bash
+
+# # Activate virtual environment if needed
+# source venv/bin/activate
+
+# # Pull the latest changes from GitHub repository
+# git pull -f origin main
+
+# # Install or update dependencies (if needed)
+# pip install -r requirements.txt
+
+# # Apply database migrations (if needed)
+# python manage.py migrate
+# python manage.py collectstatic --noinput
+# sudo systemctl restart gunicorn
+# # Restart services (if needed)
+# systemctl restart cybexplore
+
+#=======
+git add .
+git commit -m "Courses Templates"
+# git pull
+git push 
+#>>>>>>> origin/main
